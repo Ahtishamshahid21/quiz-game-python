85b1800edba57aa635c2110582a5feecbea7d031
Ahtishamshahid21 <f2023105185@umt.edu.pk>
2025-05-31 14:14:12 +0500

    Update questions.py

--- questions.py
+++ questions.py
@@ -1,7 +1,7 @@
 questions = [
-    {"question": "What is the capital of Pakistan?", "options": ["Karachi", "Lahore", "Islamabad", "Peshawar"], "answer": "Islamabad"},
-    {"question": "2 + 2 = ?", "options": ["3", "4", "5", "6"], "answer": "4"},
-    {"question": "Python is ___?", "options": ["snake", "language", "fruit", "game"], "answer": "language"},
-    {"question": "Which language is used for web development?", "options": ["Python", "C++", "HTML", "Assembly"], "answer": "HTML"},
-    {"question": "Who developed Python?", "options": ["Guido van Rossum", "Bill Gates", "Linus Torvalds", "Mark Zuckerberg"], "answer": "Guido van Rossum"}
+    {"id": 1, "question": "What is the capital of Pakistan?", "options": ["Karachi", "Lahore", "Islamabad", "Peshawar"], "answer": "Islamabad"},
+    {"id": 2, "question": "2 + 2 = ?", "options": ["3", "4", "5", "6"], "answer": "4"},
+    {"id": 3, "question": "Python is ___?", "options": ["snake", "language", "fruit", "game"], "answer": "language"},
+    {"id": 4, "question": "Which language is used for web development?", "options": ["Python", "C++", "HTML", "Assembly"], "answer": "HTML"},
+    {"id": 5, "question": "Who developed Python?", "options": ["Guido van Rossum", "Bill Gates", "Linus Torvalds", "Mark Zuckerberg"], "answer": "Guido van Rossum"}
 ]
